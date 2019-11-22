b4e9c7338217068382e3575033fa7b05cfc717b8
Leonilo Torres <torresle_sd2053@ceaccs.net>
2019-11-22 08:14:58 +0800

    final project structure

--- app/src/main/java/com/pn/groupC/dailyfaces/MainActivity.java
+++ app/src/main/java/com/pn/groupC/dailyfaces/MainActivity.java
@@ -1,17 +1,14 @@
-package com.yol.dailyfaces;
+package com.pn.groupC.dailyfaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import com.yol.adroidpermission.push_notifications.PostEvent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PostEvent evnt= new PostEvent();
-        evnt.initPusher();
     }
-
 }
