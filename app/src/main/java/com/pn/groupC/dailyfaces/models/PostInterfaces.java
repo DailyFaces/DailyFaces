09ba8d7bad9c38de41ee30d85d97adf919b8fa00
cjcabungcag <chillajean.cabungcag@student.passerellesnumeriques.org>
2019-11-25 19:17:53 +0800

    done

--- app/src/main/java/com/pn/groupC/dailyfaces/models/PostInterfaces.java
+++
@@ -1,7 +0,0 @@
-package com.pn.groupC.dailyfaces.models;
-import org.json.JSONObject;
-import java.util.*;
-public class PostInterfaces {
-    public static Deque <JSONObject> my_day = new LinkedList<>();
-    public static  Deque <JSONObject> news_feed =new LinkedList<>();
-}
